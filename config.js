5b5afb50ab9456c3ab76663680b63ceca1f0a3b8
tinnguyen0504 <88759057+tinnguyen0504@users.noreply.github.com>
2021-08-11 11:30:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     introTitle: 'tÍN ngUYỄN à!',
-    introDesc: `Trái đất vốn lạ thường
+    introDesc: `Trái đất vốSDFDSFn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
